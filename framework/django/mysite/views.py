95edd199737b4d548ae49d833337f2cb32c81376
Andrea Davanzo <info@adavanzo.com>
2026-01-29 20:44:32 +0000

    update django

--- framework/django/mysite/views.py
+++ framework/django/mysite/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Greeting
 
 def hello(request):
-    message = Greeting.fetch()
-    return HttpResponse(message, content_type="text/plain")
+    context = {'greeting': Greeting.fetch()}
+    return render(request, 'index.html', context)
