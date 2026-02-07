97c43024256fe6ff9720ec22d1fee950c0abb0a8
Andrea Davanzo <info@adavanzo.com>
2026-02-07 16:28:18 +0000

    update sinatra

--- framework/sinatra/hello.rb
+++ framework/sinatra/hello.rb
@@ -4,38 +4,11 @@
 # Copyright (c) Andrea Davanzo
 
 require 'sinatra'
-require 'pg'
 require 'erb'
 
-# Database Configuration
-DB_CONFIG = {
-  host: '192.168.37.131',
-  dbname: 'postgres',
-  user: 'tester',
-  password: 'tester'
-}
-configure do
-  set :reload_templates, true
-  set :logging, false # Logging consumes disk I/O energy, turn off for pure ACS test
-  # Tilt.default_mapping.clear_pipeline_cache! if defined?(Tilt)
-end
-
-helpers do
-  def db_query(sql)
-    conn = PG.connect(DB_CONFIG)
-    result = conn.exec(sql)
-    conn.close
-    result
-  rescue PG::Error => e
-    halt 500, "Database Error: #{e.message}"
-  end
-end
+set :environment, :production
 
 get '/' do
-  # Fetch data from DB
-  result = db_query("SELECT 'hello world' AS greeting;")
-  @greeting = result.first['greeting']
-
-  # Render the 'index.erb' template
+  @greeting = "hello world"
   erb :index
 end
