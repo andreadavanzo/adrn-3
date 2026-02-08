b68e94bfc512ac95d06c6f5c3adbf20950ca74ab
Andrea Davanzo <info@adavanzo.com>
2026-02-08 08:30:24 +0000

    update f3

--- framework/f3/index.php
+++ framework/f3/index.php
@@ -7,43 +7,17 @@
 
 require 'vendor/autoload.php';  // load F3 via Composer
 
-
-// Start session
-session_start();
-
 $f3 = \Base::instance();
 $f3->set('DEBUG', 0);
-$f3->set('CACHE', false);
-
-// PostgreSQL connection settings
-$dbhost = '192.168.37.131';
-$dbname = 'postgres';
-$dbuser = 'tester';
-$dbpass = 'tester';
-$dsn = "pgsql:host=$dbhost;dbname=$dbname";
+$f3->set('CACHE', true);
 
 // Route
-$f3->route('GET /', function($f3) use ($dsn, $dbuser, $dbpass) {
-  try {
-    // Connect to PostgreSQL
-    $pdo = new PDO($dsn, $dbuser, $dbpass, [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+$f3->route('GET /', function($f3) {
+  $greeting = 'hello world';
 
-    // Execute query
-    $stmt = $pdo->query("SELECT 'Hello World' AS greeting;");
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $greeting = $row['greeting'] ?? 'error';
+  $f3->set('greeting', $greeting);
+  echo \Template::instance()->render('template.html');
 
-    // Set variable for template
-    $f3->set('greeting', $greeting);
-
-    // Render template
-    echo \Template::instance()->render('template.html');
-
-  } catch (PDOException $e) {
-    echo "DB connection failed: " . $e->getMessage();
-  }
 });
 
 $f3->run();
