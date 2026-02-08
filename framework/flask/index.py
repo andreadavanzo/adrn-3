e7afc1c5661aea042eb6f8dbf5a9061c49136361
Andrea Davanzo <info@adavanzo.com>
2026-02-08 08:16:17 +0000

    update flask

--- framework/flask/index.py
+++ framework/flask/index.py
@@ -6,32 +6,12 @@
 # Copyright (c) Andrea Davanzo
 
 from flask import Flask, render_template
-import psycopg2
 
 app = Flask(__name__)
 
-# PostgreSQL connection settings
-dbhost = '192.168.37.131'
-dbname = 'postgres'
-dbuser = 'tester'
-dbpass = 'tester'
-
 @app.route("/")
 def hello():
-    conn = psycopg2.connect(
-        host=dbhost,
-        dbname=dbname,
-        user=dbuser,
-        password=dbpass
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT 'hello world' AS greeting;")
-    row = cur.fetchone()
-    cur.close()
-    conn.close()
-
-    # Pass the database result to the HTML template
-    greeting_text = row[0] if row else 'error'
+    greeting_text = 'hello world'
     return render_template('index.html', greeting=greeting_text)
 
 if __name__ == "__main__":
