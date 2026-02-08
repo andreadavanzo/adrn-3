372b6648ae61edbb79bba5ad681edb8a66792d46
Andrea Davanzo <info@adavanzo.com>
2026-02-08 08:24:11 +0000

    update fastify

--- framework/fastify/controllers/greetingController.js
+++ framework/fastify/controllers/greetingController.js
@@ -3,19 +3,9 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (c) Andrea Davanzo
 
-const { getGreeting } = require('../models/greetingModel');
-
-async function getGreetingHandler(request, reply) {
-  try {
-    const row = await getGreeting();
-    const message = row?.greeting || 'error';
-
-    // 3. Render the template
+function getGreetingHandler(request, reply) {
+    const message = 'hello world';
     return reply.view('index.ejs', { greeting: message });
-  } catch (err) {
-    console.error(err);
-    reply.status(500).send('DB connection failed');
-  }
 }
 
 module.exports = { getGreetingHandler };
