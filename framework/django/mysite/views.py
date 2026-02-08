2db978a27181f8706b3652e4386d810aa3ec6453
Andrea Davanzo <info@adavanzo.com>
2026-02-08 08:47:33 +0000

    update .gitignore

--- framework/django/mysite/views.py
+++ framework/django/mysite/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import Greeting
 
 def hello(request):
-    context = {'greeting': Greeting.fetch()}
+    context = {'greeting': 'hello world'}
     return render(request, 'index.html', context)
